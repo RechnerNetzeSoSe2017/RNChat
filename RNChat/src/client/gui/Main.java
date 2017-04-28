7a3d3102b6f47372e88701b5e717edade73e4520
Schorzz <Uh2stos!>
2017-04-28 21:16:26 +0200

    Client hinzugefügt

---
+++ RNChat/src/client/gui/Main.java
@@ -0,0 +1,31 @@
+package client.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage){
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("MainUI.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+}
