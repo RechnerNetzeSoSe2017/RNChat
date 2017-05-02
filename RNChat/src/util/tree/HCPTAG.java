6595dc1f6e4ebf7a674b9fd6c5850d8a52b878c8
schorzz <sir.lightness@gmx.de>
2017-05-02 12:58:05 +0200

    TAG Libary für HCP

---
+++ RNChat/src/util/tree/HCPTAG.java
@@ -0,0 +1,32 @@
+package util.tree;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class HCPTAG {
+
+	private static HashMap<String, String> openZUcloseTAG = new HashMap<>();
+
+	static {
+		openZUcloseTAG.put("<message>", "</message>");
+		
+		openZUcloseTAG.put("<nameservice>", "</nameservice>");
+		openZUcloseTAG.put("<changenick>", "</changenick>");
+		openZUcloseTAG.put("<nickid>", "</nickid>");
+		
+		openZUcloseTAG.put("<control>", "</control>");
+		
+	}
+
+	/**
+	 * 
+	 * @param tag
+	 * @return closeTAG zu tag, null wenn es keinen SchlieﬂTAG gibt (also auch wenn es den OpenTAG nicht gibt)
+	 */
+	public static String getCloseTAG(String tag) {
+
+		return openZUcloseTAG.get(tag);
+		
+	}
+
+}
---
+++ RNChat/src/util/tree/PayloadBuilder.java
@@ -0,0 +1,21 @@
+package util.tree;
+
+public class PayloadBuilder {
+	
+	private String closeTAG = ">";
+	
+	public Payload getFromString(String string){
+		
+		String arbeitsString = string.toLowerCase().trim();
+		int firsTAGIndex = arbeitsString.indexOf(closeTAG);
+		String praefix = arbeitsString.substring(0, firsTAGIndex);
+		if()
+		
+		
+		return null;
+	}
+	private String getBetweenTAGs(String startTAG, String endTAG){
+		return null;
+	}
+
+}
