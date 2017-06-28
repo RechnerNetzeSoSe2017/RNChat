41a77b6a680fa7fae492edadbb45c20dce587d4b
Schorzz <Uh2stos!>
2017-06-28 09:45:22 +0200

    anderer Server

---
+++ RNChat/src/client/protocolother/MainGui.java
@@ -0,0 +1,31 @@
+package client.protocolother;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainGui extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("MainUI.fxml"));
+//			Scene scene = new Scene(root, 400, 400);
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
